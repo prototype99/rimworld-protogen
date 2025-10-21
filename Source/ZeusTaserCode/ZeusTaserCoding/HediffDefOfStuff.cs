963bdfa17a98c5204247cbef0a332f2f62453812
Seirra <sophietheopossum@yandex.ru>
2025-10-21 16:32:19 +0100

    Initial commit

---
+++ Source/ZeusTaserCode/ZeusTaserCoding/HediffDefOfStuff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace HediffDefOfStuff
+{
+    #region Overrides
+    [DefOf]
+    public static class HediffDefOf
+    {
+        public static HediffDef TaserShock;
+    }
+    #endregion Overrides
+}
