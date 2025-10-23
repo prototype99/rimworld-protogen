a7f01f88147b1ff4fa8a0ba231b52cd078a90fd7
Seirra <sophietheopossum@yandex.ru>
2025-10-23 21:07:02 +0100

    change default value
    apparently xml is invalid, but value is only set once.... might as well just change the default

--- Source/ZeusTaserCode/ZeusTaserCoding/TaserCoding.cs
+++ Source/ZeusTaserCode/ZeusTaserCoding/TaserCoding.cs
@@ -4,7 +4,7 @@ namespace Zeus
 {
     public class ThingDef_ZeusShock : ThingDef
     {
-        public const float AddHediffChance = 1f; //Default chance of adding hediff.
+        public const float AddHediffChance = 0.9f; //Default chance of adding hediff.
         public HediffDef HediffToAdd;
 
         public override void ResolveReferences()
